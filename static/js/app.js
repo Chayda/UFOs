16875ca5d972e671235766a76a67365eeccb867c
Chayda Adora <chayda.adora@gmail.com>
2021-09-02 10:13:08 -0400

    Table Created.

--- static/js/app.js
+++ static/js/app.js
@@ -21,3 +21,24 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+// Check to see if a date was entered; return only the data with that date
+    if (date) {
+        // Apply filter method to the dtable data to only keep the rows where the 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Rebuild the table using the filtered data @NOTE: If no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+builtTable(tableData);
